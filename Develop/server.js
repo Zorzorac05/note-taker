dc54904d1754bf231f739f21b46e12080df2d5af
Alex Ryder <ajryder31@gmail.com>
2024-03-14 22:22:58 -0600

    able to delete items from the json file

--- Develop/server.js
+++ Develop/server.js
@@ -3,8 +3,9 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 let noteData = require('./db/db.json');
+//const uuid = require('./helpers/uuid');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -16,7 +17,7 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
-//notes page
+//notes page route
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
@@ -24,23 +25,49 @@ app.get('/notes', (req, res) =>
 //gets note data
 app.get('/api/notes', (req, res) => res.json(noteData));
 
+//delete notes
+app.delete(`/api/notes/:id`, (req, res) => {
+  console.log("delete request called" + req.params.id);
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // Convert string into JSON object
+      let parsedNotes = JSON.parse(data);
+      
+      parsedNotes = parsedNotes.filter(function( obj ) {
+        return obj.id !== req.params.id;
+      });
+      console.log(parsedNotes);
+      noteData = parsedNotes;
+
+      // Write updated reviews back to the file
+      fs.writeFile(
+        './db/db.json',
+        JSON.stringify(parsedNotes, null, 4),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('updated notes')
+      );
+    }
+  });
+});
 
 app.post('/api/notes', (req, res) => {
   // Log that a POST request was received
   console.info(`${req.method} request received to add a review`);
-
+  const id = Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
   //checks if fields are filled if they are makes a new note object
   const {title, text} = req.body;
   if(title && text) {
     const newNote = {
       title,
       text,
+      id,
     };
-
-    //convert object to a string for storing
-    //const noteString = JSON.stringify(newNote);
-    //console.log(noteString);
-
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) {
         console.error(err);
@@ -48,8 +75,9 @@ app.post('/api/notes', (req, res) => {
         // Convert string into JSON object
         const parsedNotes = JSON.parse(data);
 
-        // Add a new review
+        // Add a new note to the json file
         parsedNotes.push(newNote);
+        //updates note data with changes to json file
         noteData = parsedNotes;
 
         // Write updated reviews back to the file
@@ -59,7 +87,7 @@ app.post('/api/notes', (req, res) => {
           (writeErr) =>
             writeErr
               ? console.error(writeErr)
-              : console.info('Successfully updated reviews!')
+              : console.info('updated notes')
         );
       }
     });
